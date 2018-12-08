8eaa7da7864922987e73519a9e9992352819ab46
thefyrewire <michael-hay369@hotmail.co.uk>
2018-12-08 20:25:40 +0000

    login now uses model gateway

--- KF7012_Assignment/Login/LoginPresenter.cs
+++ KF7012_Assignment/Login/LoginPresenter.cs
@@ -25,11 +25,7 @@ namespace KF7012_Assignment
             // updateView();
 
             // DB TEST
-            model.deleteDatabase();
-            model.addUser(0000, "w170", "pass", "user");
-            model.addUser(0001, "w171", "pass", "user");
-            model.addUser(0002, "w172", "pass", "user");
-            model.addUser(0003, "w173", "pass", "user");
+            model.resetDatabase();
         }
 
         /*public void updateView()
@@ -47,16 +43,10 @@ namespace KF7012_Assignment
 
             if (!String.IsNullOrEmpty(screen.getAttemptUsername()) && !String.IsNullOrEmpty(screen.getAttemptPassword()))
             {
-                if (screen.getAttemptUsername() == "admin" && screen.getAttemptPassword() == "admin")
-                {
-                    screen.showMainForm();
-                }
-                else
-                {
-                    Console.WriteLine("Wrong password, dummy!");
-                    screen.showError("Incorrect username or password.");
-                }
-
+                bool verified = model.verifyUser(screen.getAttemptUsername(), screen.getAttemptPassword());
+                
+                if (verified) screen.showMainForm();
+                else screen.showError("Incorrect username or password.");
             }
             else
             { 
--- KF7012_Assignment/Model/ModelGateway.cs
+++ KF7012_Assignment/Model/ModelGateway.cs
@@ -14,8 +14,22 @@ namespace KF7012_Assignment
             using (var context = new Model()) { context.Database.Delete(); }
         }
 
+        public void resetDatabase()
+        {
+            this.deleteDatabase();
 
+            // ADD USERS
+            this.addUser(0000, "admin", "admin", "admin");
+            this.addUser(1701, "w1701", "pass", "user");
+            this.addUser(1702, "w1702", "pass", "user");
+            this.addUser(1703, "w1703", "pass", "user");
+            this.addUser(1704, "w1704", "pass", "user");
+        }
+
+        /* ------------- */
         /* --- USERS --- */
+        /* ------------- */
+
         public void addUser(int userID, string username, string password, string profile)
         {
             using (var context = new Model())
@@ -38,6 +52,19 @@ namespace KF7012_Assignment
             {
                 var users = context.Users.ToList<User>();
                 return users;
+            }
+        }
+
+        public bool verifyUser(string username, string password)
+        {
+            using (var context = new Model())
+            {
+                List<User> users = this.getUsers();
+                User userToVerify = users.Where(user => user.username == username && user.password == password).FirstOrDefault<User>();
+                if (userToVerify != null)
+                    return true;
+
+                return false;
             }
         }
     }
